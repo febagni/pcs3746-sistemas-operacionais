733ff3b8bdab55347698ffd7062dc013995a9f9d
Rafael Yokowo <rafael.yokowo@usp.br>
2022-06-18 09:39:53 -0300

    :sparkles: adding producer-consumer test case in user mode

--- 1/initramfs/semaphore.c
+++ 1/initramfs/semaphore.c
@@ -13,9 +13,9 @@
 
 int buffer[N] = {-1,-1,-1,-1,-1};
 
-long* mutex = init_semaphore(1);
-long* empty = init_semaphore(N);
-long* full = init_semaphore(0);
+int * mutex;
+int * empty;
+int * full;
 
 void print_buffer();
 int produce_item();
@@ -28,10 +28,12 @@ void *consumer();
 
 int main()
 {   
+    mutex = init_semaphore(1);
+    empty = init_semaphore(N);
+    full = init_semaphore(0);
 	print_buffer();
     pthread_t threads[NUM_THREADS];
     int rc;
-    long t;
     printf("In main: creating thread consumer \n");
     rc = pthread_create(&threads[0], NULL, consumer, (void *)0);
     if (rc){
@@ -65,6 +67,7 @@ int produce_item() {
 }
 
 void insert_item(int item){
+    print_buffer();
     if (buffer[N-1] != -1){
         printf("Buffer cheio! Item %d descartado \n", item);
         return;
@@ -73,23 +76,28 @@ void insert_item(int item){
         for(int i=0; i<N; i++){
             if (buffer[i] == -1){
                 buffer[i] = item;
+                print_buffer();
                 return;
             }
         }
         return;
     }
+    print_buffer();
 }
 
 int remove_item() {
     int item;
+    print_buffer();
     if(buffer[0] == -1){
         printf("Buffer vazio, não há o que consumir! \n");
+        print_buffer();
         return -1;
     }
     for(int i=0; i < N; i++){
         if (buffer[N-i] != -1) {
             item = buffer[N-1];
             buffer[N-1] = -1;
+            print_buffer();
             return item;
         }
     }
@@ -108,11 +116,15 @@ void *producer(void){
 
     while(1){
         item = produce_item();
-        down(&empty);
-        down(&mutex);
+        down(empty);
+        down(mutex);
+        printf("Produtor entrando na região crítica! \n");
         insert_item(item);
-        up(&mutex);
-        up(&full);
+        up(mutex);
+        up(full);
+        printf("Produtor saindo da região crítica! \n");
+        fflush(stdout);
+        sleep(3);
     }
 }
 
@@ -120,11 +132,15 @@ void *consumer(void){
     int item;
 
     while(1){
-        down(&full);
-        down(&mutex);
+        down(full);
+        down(mutex);
+        printf("Consumidor entrando na região crítica! \n");
         item = remove_item();
-        up(&mutex);
-        up(&empty);
+        up(mutex);
+        up(empty);
+        printf("Consumidor saindo da região crítica! \n");
         consume_item(item);
+        fflush(stdout);
+        sleep(3);
     }
 }
